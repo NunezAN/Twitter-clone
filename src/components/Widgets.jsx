3cad2931002d8a26d125662e66de0d0ff083c7dc
NunezAN <alex101nunez@gmail.com>
2022-08-25 22:38:10 -0500

    completed styling of widgets panel

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import "./Widgets.css"
+import React from "react";
+import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Widgets = () => {
-    return (
-        <div className='widgets'>
-            <div className="widgets__wh">
-                Widgets
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__SearchIcon" />
+        <input className="widgets__input--input" type="text" placeholder="Search Twitter" />
+      </div>
+      <div className="widgets__wh">
+        <h2 className="widgets__wh--title">What's Happening</h2>
+        <TwitterTimelineEmbed
+          theme="dark"
+          sourceType="profile"
+          screenName="motivational"
+          autoHeight ={true}
+          tweetLimit = {2}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Widgets;
